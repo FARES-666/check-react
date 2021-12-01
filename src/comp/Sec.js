558774949af3e7e964e01dd8ace5bdfd08e76973
fares fakhfakh <faresfakhfakh97@gmail.com>
2021-12-01 14:31:02 +0100

    first commit

---
+++ src/comp/Sec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Sec.css';
+export const Sec = () => {
+    return (
+        <div>
+            <img  className='img' src="https://th.bing.com/th/id/OIP.w_TsEm0ozE9YAmCKfbmx8wHaJ9?pid=ImgDet&rs=1" alt="" />
+        </div>
+    )
+}
